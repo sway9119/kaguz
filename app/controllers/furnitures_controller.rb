edb344a1af871d9f16d359b247cf88fef8300ee3
sway <40759948+sway9119@users.noreply.github.com>
2022-01-07 23:37:35 +0900

    家具作成の追加

--- app/controllers/furnitures_controller.rb
+++ app/controllers/furnitures_controller.rb
@@ -1,2 +1,23 @@
 class FurnituresController < ApplicationController
+
+    layout "home"
+
+    def new
+        @furniture = Furniture.new
+    end
+
+    def create
+        @furniture = Furniture.new(furniture_params)
+        @furniture.save
+    end
+
+    def show
+        @furniture = Furniture.find(params[:id])
+    end
+
+    private
+
+    def furniture_params
+        params.require(:furniture).permit(:name)
+    end
 end
