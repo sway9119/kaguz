9afa608f428284426c73b29c1b8e8f65753d812d
sway <40759948+sway9119@users.noreply.github.com>
2022-01-18 00:11:33 +0900

    カテゴリー件数の表示機能の追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,17 @@
 class HomeController < ApplicationController
+    before_action :get_categories_for_sidebar
 
     layout "home"
 
     def index
         @furnitures = Furniture.all.order(created_at: "DESC")
-        @categories = Category.all
     end
 
     def category_search
         @furnitures = Furniture.where(category_id: params[:category_id])
-        @categories = Category.all
     end
 
+    def get_categories_for_sidebar
+        @categories_hash = Furniture.joins(:category).select("category_id, categories.name as category_name, count(*) as category_count").group(:category_id, :category_name)
+    end
 end
