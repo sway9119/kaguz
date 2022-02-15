2231a9f31dc370d883ba14d34a22e50717446728
sway <40759948+sway9119@users.noreply.github.com>
2022-02-15 21:27:27 +0900

    家具画像の修正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -30,7 +30,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process resize_to_fit: [100, 100]
+    process resize_to_fill: [100, 100, "Center"]
   end
 
   # Add an allowlist of extensions which are allowed to be uploaded.
