f1f1c209833b96988362d73409c05d4e51362d8d
sway <40759948+sway9119@users.noreply.github.com>
2022-02-17 20:52:53 +0900

    AdminUsersテーブルの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_29_054912) do
+ActiveRecord::Schema.define(version: 2022_02_16_123126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admin_users", force: :cascade do |t|
+    t.string "name", null: false, comment: "ユーザー名称"
+    t.string "email", null: false, comment: "メールアドレス"
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "name", null: false, comment: "カテゴリー名称"
