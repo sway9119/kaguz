3ea9ee0df9ca1d28fd26117190cdd242e8d11208
sway <40759948+sway9119@users.noreply.github.com>
2022-02-24 23:02:24 +0900

    LP用にhomeページを家具一覧に修正

--- app/controllers/furnitures_controller.rb
+++ app/controllers/furnitures_controller.rb
@@ -1,6 +1,30 @@
 class FurnituresController < ApplicationController
+    before_action :get_categories_for_sidebar
+    before_action :get_scenes_for_sidebar
 
     layout "home"
+
+    def index
+        @furnitures = Furniture.all.order(created_at: "DESC")
+    end
+
+    def category_search
+        @furnitures = Furniture.where(category_id: params[:category_id])
+        render 'index'
+    end
+
+    def scene_search
+        @furnitures = Scene.find(params[:scene_id]).furnitures
+        render 'index'
+    end
+
+    def get_categories_for_sidebar
+        @categories_hash = Furniture.joins(:category).select("category_id, categories.name as category_name, count(*) as category_count").group(:category_id, :category_name)
+    end
+
+    def get_scenes_for_sidebar
+        @scenes_hash = Furniture.joins(:scenes).select("scene_id, scenes.name as scene_name, count(*) as scene_count").group(:scene_id, :scene_name)
+    end
 
     def new
         @furniture = Furniture.new
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
-  get 'notifications/show'
+
   root to: "home#index"
 
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-
+  
   get    '/signup',  to: 'users#new'
   post   '/signup',  to: 'users#create'
   get    '/mypage',  to: 'users#show'
+  get 'notifications/show'
 
   # 管理画面
   namespace :admin do
@@ -20,12 +21,13 @@ Rails.application.routes.draw do
   resources :notifications  
   resources :users
 
-  resources :home do
+  resources :home
+
+  resources :furnitures do
     get :category_search, on: :collection
     get :scene_search, on: :collection
   end
 
-  resources :furnitures
   resources :workshops
 
 end
