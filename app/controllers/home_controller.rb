a3dc69554b2d88daf8ed21b5f41603593367ae8d
sway <40759948+sway9119@users.noreply.github.com>
2022-02-28 20:02:02 +0900

    LP用カテゴリ欄の追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,12 @@
 class HomeController < ApplicationController
+    before_action :get_categories_for_sidebar
 
     layout 'home'
 
     def index
     end
 
+    def get_categories_for_sidebar
+        @categories_hash = Furniture.joins(:category).select("category_id, categories.name as category_name, count(*) as category_count").group(:category_id, :category_name)
+    end
 end
