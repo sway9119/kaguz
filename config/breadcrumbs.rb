ae65461e39be148da0f76c4024f7945e067f2089
sway <40759948+sway9119@users.noreply.github.com>
2022-03-30 12:09:43 +0900

    家具詳細のメイン画像欄を追加

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -1,3 +1,8 @@
+# crumb "ページ名" do
+#   link "ビューに表示される名前", "リンクされるURL" //_pathは書かないこと
+#   parent :親のページ名（現在の前のページ）
+# end
+
 crumb :root do
   link "ホーム", root_path
 end
@@ -7,27 +12,7 @@ crumb :furnitures_search do
   parent :root
 end
 
-# crumb :projects do
-#   link "Projects", projects_path
-# end
-
-# crumb :project do |project|
-#   link project.name, project_path(project)
-#   parent :projects
-# end
-
-# crumb :project_issues do |project|
-#   link "Issues", project_issues_path(project)
-#   parent :project, project
-# end
-
-# crumb :issue do |issue|
-#   link issue.title, issue_path(issue)
-#   parent :project_issues, issue.project
-# end
-
-# If you want to split your breadcrumbs configuration over multiple files, you
-# can create a folder named `config/breadcrumbs` and put your configuration
-# files there. All *.rb files (e.g. `frontend.rb` or `products.rb`) in that
-# folder are loaded and reloaded automatically when you change them, just like
-# this file (`config/breadcrumbs.rb`).
+crumb :furnitures_show do
+  link "家具詳細", :furnitures
+  parent :furnitures_search
+end
