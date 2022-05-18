a772aad590ee731e6e8e8a4bab0b5af44a24044a
sway <40759948+sway9119@users.noreply.github.com>
2022-05-18 19:46:26 +0900

    仮機能（素材一覧表示, 色一覧表示）を削除

--- app/controllers/furnitures_controller.rb
+++ app/controllers/furnitures_controller.rb
@@ -41,50 +41,16 @@ class FurnituresController < ApplicationController
         render 'index'
     end
 
+    def keyword_search 
+
+    end
+
     def get_categories_for_sidebar
         @categories_hash = Furniture.joins(:category).select("category_id, categories.name as category_name, count(*) as category_count").group(:category_id, :category_name)
     end
 
     def get_scenes_for_sidebar
         @scenes_hash = Furniture.joins(:scenes).select("scene_id, scenes.name as scene_name, count(*) as scene_count").group(:scene_id, :scene_name)
-    end
-
-    def get_material_for_sidebar
-        @materials = [
-            "木材",
-            "金属",
-            "石材",
-            "テキスタイル",
-            "革・レザー",
-            "ガラス",
-            "陶磁器",
-            "土木材",
-            "紙",
-            "左官材",
-            "メラミン・樹脂・プラスチック",
-            "その他"
-        ]
-    end
-
-    def get_color_for_sidebar
-        @colors_hash = [
-            {id:  1, name: "レッド", klass: "sidebar-color sidebar-color_red"},
-            {id:  2, name: "オレンジ", klass: "sidebar-color sidebar-color_orange"},
-            {id:  3, name: "イエロー", klass: "sidebar-color sidebar-color_yellow"},
-            {id:  4, name: "グリーン", klass: "sidebar-color sidebar-color_green"},
-            {id:  5, name: "ブルー", klass: "sidebar-color sidebar-color_blue"},
-            {id:  6, name: "パープル", klass: "sidebar-color sidebar-color_purple"},
-            {id:  7, name: "ピンク", klass: "sidebar-color sidebar-color_pink"},
-            {id:  8, name: "ブラウン", klass: "sidebar-color sidebar-color_brown"},
-            {id:  9, name: "ブラウン", klass: "sidebar-color sidebar-color_brown"},
-            {id: 10, name: "ベージュ", klass: "sidebar-color sidebar-color_beige"},
-            {id: 11, name: "ホワイト", klass: "sidebar-color sidebar-color_white"},
-            {id: 12, name: "グレー", klass: "sidebar-color sidebar-color_grey"},
-            {id: 13, name: "ブラック", klass: "sidebar-color sidebar-color_black"},
-            {id: 14, name: "ゴールド", klass: "sidebar-color sidebar-color_gold"},
-            {id: 15, name: "クリア", klass: "sidebar-color sidebar-color_clear"},
-            {id: 16, name: "マルチ", klass: "sidebar-color sidebar-color_multi"}
-        ]
     end
 
     def new
