3735bbe2cbb014f1aa40e06d199f48678008c22e
sway <40759948+sway9119@users.noreply.github.com>
2023-10-25 21:38:30 +0900

    Update furniture_scene.rb

--- app/models/furniture_scene.rb
+++ app/models/furniture_scene.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
+# 家具とシーンのモデル
 class FurnitureScene < ApplicationRecord
-    belongs_to :furniture
-    belongs_to :scene
+  belongs_to :furniture
+  belongs_to :scene
 end
